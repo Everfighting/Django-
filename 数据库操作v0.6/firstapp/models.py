f274db039010ef1ba1c6f1983ca8ad8327db9097
Everfighting <cbb903601682@163.com>
2017-02-10 14:49:18 +0800

    add an update version

---
+++ 数据库操作v0.6/firstapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField(null=True, blank=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    createtime = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    name = models.CharField(max_length=10)
+    content = models.TextField()
+    createtime = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.content
